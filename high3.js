868e2f246258fd53d58f93a96eac8fbd78bd67f0
ninjaa <alkhudrisaid83@gmail.com>
2020-09-18 09:56:39 +0700

    first commit

---
+++ high3.js
@@ -0,0 +1,27 @@
+var kodeUnixRandom = [
+  12,
+  78,
+  42,
+  40,
+  36,
+  2,
+  8,
+  96,
+  66,
+  20,
+  91,
+  85,
+  41,
+  69,
+  38,
+  77,
+  97,
+  60,
+  15,
+  57,
+];
+const newKodeUnix = kodeUnixRandom.reduce(
+  (accumulator, currentValue) =>
+    (accumulator + currentValue) / kodeUnixRandom.length
+);
+console.log(newKodeUnix);
